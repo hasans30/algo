0dcb62ceba7b5e793806c0474fb48dc299cc2f69
Hasanuzzaman <hasan@live.in>
2016-01-26 23:17:36 -0800

    unique char sequence added

---
+++ DSandAlgo/PrintZigZag.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgo
+{
+    class PrintZigZag
+    {
+        public static void printZigzag(Node root) {
+  List queue = new List();
+  Stack stack = new Stack();
+  if (root == null)
+   return;
+  Node marker = new Node(-1);
+  boolean printOrder = true;
+  queue.add(root);
+  queue.add(marker);
+  while (!queue.isEmpty()) {
+   Node node = queue.poll();
+   if (node == marker) {
+    if (!printOrder) {
+     while (stack.size() > 0) {
+      Node printNode = stack.pop();
+      System.out.println(printNode.value);
+
+     }
+
+    }
+    printOrder = !printOrder;
+    if (!queue.isEmpty())
+     queue.add(marker);
+    continue;
+   }
+   if (printOrder) {
+    System.out.println(node.value);
+   } else {
+    stack.push(node);
+   }
+   if (node.left != null)
+    queue.add(node.left);
+   if (node.right != null)
+    queue.add(node.right);
+
+  }
+
+ }
+
+    }
+}
---
+++ DSandAlgo/UniqueCharSequence.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSandAlgo
+{
+    class UniqueCharSequence
+    {
+        public void UCSequence()
+        {
+            int si = 0, ei = 0, ms = 0, me = 0, ml = 0, cl = 0;
+            string str = "";
+            HashSet<char> h = new HashSet<char>();
+            for (ei = 0; ei < str.Length; ei++)
+            {
+                if (!h.Contains(str[ei]))
+                {
+                    h.Add(str[ei]);
+                    cl = ei - si + 1;
+                    if (cl > ml)
+                    {
+                        ms = si;
+                        me = ei;
+                        ml = cl;
+                    }
+
+                }
+                else
+                {
+                    si = ei;
+                    //empty the hashset
+                    h.Clear();
+                    //add si
+                    h.Add(str[si]);
+
+                }
+
+            }
+            Console.WriteLine("{0},{1}", ms, me);
+            for (int i = ms; i <= me; i++)
+                Console.Write(str[i]);
+        }
+    }
+}
